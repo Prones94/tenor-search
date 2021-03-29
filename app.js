e9169e83de496ce817f0c34ad73570e9f333a5ee
Ian Rones <ronesp.85@gmail.com>
2021-03-29 16:25:39 -0700

    [add] css styles

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000
 
+app.use(express.static('public'));
+
 // Libraries
 const Tenor = require("tenorjs").client({
   "Key": "GU8W2OSB2NSD",
